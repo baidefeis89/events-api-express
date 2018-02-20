8491d3b052147eb0b1a6cb6a82a2b541de0a1059
Ivan <baidefeis89@gmail.com>
2018-02-20 18:34:59 +0100

    bugfix static server

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use('/', express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', index);
 app.use('/events', passport.authenticate('jwt', {session: false}), events);
